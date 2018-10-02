c77b815b6cbecc4382fe204624414f443d1f8cde
52fisher <tiebayu@126.com>
2018-10-02 20:02:19 +0800

    增加检测更新

---
+++ 567pan.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         多网盘直接下载
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  无需验证码以及等待时间,直接跳转至下载页面。注意：还是无法实现会员功能
+// @author       fisher
+// @match        http://www.567pan.com/*
+// @match        http://www.520file.com/*
+// @grant        none
+// ==/UserScript==
--- 567pan.user.js
+++ 567pan.user.js
@@ -4,6 +4,8 @@
 // @version      0.2
 // @description  无需验证码以及等待时间,直接跳转至下载页面。注意：还是无法实现会员功能
 // @author       fisher
+// @updateURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/567pan.meta.js
+// @downloadUR   https://gitee.com/fisher52/Anti-CloudDisk/raw/master/567pan.user.js
 // @match        http://www.567pan.com/*
 // @match        http://www.520file.com/*
 // @grant        none
