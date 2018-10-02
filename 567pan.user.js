f2fcb5942cb660422faaf746aeede25feb73c72e
52fisher <tiebayu@126.com>
2018-10-02 19:46:19 +0800

    修复失效问题

--- 567pan.user.js
+++ 567pan.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
-// @name         567网盘直接下载
+// @name         多网盘直接下载
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  无需验证码以及等待时间,直接跳转至下载页面。注意：还是无法实现会员功能
-// @updateURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/js/567pan.user.js
-// @downloadURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/js/567pan.user.js
 // @author       fisher
 // @match        http://www.567pan.com/*
+// @match        http://www.520file.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     var url = location.href,
-        rota = url.match(/567pan\.com\/(.*?)-(\d+)/),
+        rota = url.match(/\/([A-z]+)-(\d+)/),
     urlRota = ["file","down","down2"],
     path  = urlRota.indexOf(rota[1]),
         num = rota[2];
