0a905d563139901edf0e49f1aaebd5b87851e293
fisher <tiebayu@126.com>
2019-01-23 00:22:56 +0800

    支持更多网盘

--- 567pan.meta.js
+++ 567pan.meta.js
@@ -8,3 +8,38 @@
 // @match        http://www.520file.com/*
 // @grant        none
 // ==/UserScript==
+//  //     var url = location.pathname,
+//         rota = url.match(/([A-z]+)-(\d+)/),
+//         urlRota = ["file", "down", "down2"],
+//         path = urlRota.indexOf(rota[1]),
+//         num = rota[2];
+//     if (path != -1) {
+//         if (urlRota[path] == "file") {
+//             window.location.href = "/down-" + num + ".html";
+//         } else if (urlRota[path]) {
+//             switch (location.host) {
+//                 case "www.520file.com":
+//                     try {
+//                         document.getElementsByClassName("viplist")[0].style.display = "none";
+//                         document.getElementById("down_box").style.display = "block";
+//                     } catch (e) {
+//                         console.log(e.message);
+//                     }
+//                     break;
+//                     break;
+//                 case "www.520file.com":
+//                     $("#down_link a").attr('onclick', "abox('downbox.php?file_id=" + num + "','文件下载',480,350);")
+//                     break;
+//                 case "www.567pan.com":
+//                     try {
+//                         document.getElementsByClassName("viplist")[0].style.display = "none";
+//                         document.getElementById("down_box").style.display = "block";
+//                     } catch (e) {
+//                         console.log(e.message);
+//                     }
+//                     break;
+//                 default:
+//                     ;
+//             }
+//         }
+//     }
---
+++ yunpan.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         多网盘直接下载
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  无需验证码以及等待时间,直接跳转至下载页面。注意：还是无法实现会员功能
+// @author       fisher
+// @match        http://www.567pan.com/*
+// @match        http://www.xun-niu.com/*
+// @match        http://www.520file.com/*
+// @match        http://www.eos-53.com/*
+// @match        http://www.ccchoo.com/*
+// @match        http://page2.dfpan.com/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    var disk = {
+        zV2Public: function() {
+            redirectDownPage();
+        },
+        pdzpublic: function() {
+            var url = location.pathname,
+                rota = url.match(/(\w+)-(\d+)/),
+                urlRota = ["file", "down", "down2"],
+                path = urlRota.indexOf(rota[1]),
+                num = rota[2];
+            if (path == -1) {
+                console.log('当前方案未匹配');
+                return;
+            }
+            if (urlRota[path] == "file") {
+                window.location.href = "/down-" + num + ".html";
+            }
+            $(".viplist:eq(0)").hide()
+            $("#down_box").show()
+        },
+    };
+    var urlLists = {
+        'www.eos-53.com': function() {
+            disk.zV2Public();
+        },
+        'www.567pan.com': function() {
+            disk.pdzpublic();
+        },
+        'www.ccchoo.com': function() {
+            disk.pdzpublic();
+        },
+        'page2.dfpan.com': function() {
+            disk.pdzpublic();
+        },
+        'www.xun-niu.com': function() {
+            disk.pdzpublic();
+        }
+    }
+    var domain = location.host;
+    if (!urlLists.hasOwnProperty(domain)) {
+        console.log('当前网站暂不支持');
+        return;
+    }
+    urlLists[domain]();
+})();
