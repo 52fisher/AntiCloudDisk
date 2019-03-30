a96705aa02ffbc423074c061e36f80e772e6283c
fisher <tiebayu@126.com>
2019-03-30 21:02:00 +0800

    增加彩虹云其他域名

--- yunpan.user.js
+++ yunpan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         多网盘直接下载
 // @namespace    https://github.com/52fisher
-// @version      2.1
+// @version      2.2
 // @description  国内的一些并不出名网盘靠着xx服务赚钱，但是有时总是太过分.本脚本对一些网盘的功能进行重写，使得用户可以达到直接跳转下载页面、跳过验证码、免等待时间、兼容常见浏览器等功能.注意：无法实现会员下载，加速请支持正版
 // @author       fisher
 // @updateURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/yunpan.user.js
@@ -12,6 +12,7 @@
 // @match        http://www.eos-53.com/*
 // @match        http://www.ccchoo.com/*
 // @match        http://page2.dfpan.com/*
+// @match        http://www.mm222.cn/*
 // @grant        none
 // ==/UserScript==
 (function() {
@@ -36,7 +37,7 @@
                 window.location.href = "/down-" + num + ".html";
             }
             $(".viplist:eq(0)").hide()
-            $("#down_box").show()
+            $("#down_box,#down_link,#down_boxc").show()
             return this;
         },
         hookClick: function() {
@@ -49,25 +50,29 @@
         }
     };
     var urlLists = {
-        'www.eos-53.com': function() {
+        'www.eos-53.com': function() {//520盘
             disk.zV2Public().hookClick();
         },
-        'www.567pan.com': function() {
+        'www.567pan.com': function() {//567盘
             disk.pdzpublic();
         },
-        'www.ccchoo.com': function() {
+        'www.ccchoo.com': function() {//彩虹云
             disk.pdzpublic();
         },
-        'page2.dfpan.com': function() {
+        'page2.dfpan.com': function() {//yunfile
             disk.pdzpublic();
         },
-        'www.xun-niu.com': function() {
+        'www.xun-niu.com': function() {//讯牛
+            disk.pdzpublic();
+        },'www.mm222.cn':function(){//彩虹云别称
+            disk.pdzpublic();
+        },'www.wodech.com':function(){//彩虹云别称
             disk.pdzpublic();
         }
     }
     var domain = location.host;
     if (!urlLists.hasOwnProperty(domain)) {
-        console.log('当前网站暂不支持');
+        console.warn('当前网站暂不支持');
         return;
     }
     urlLists[domain]();
