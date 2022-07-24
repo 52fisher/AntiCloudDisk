40717f60b5db1bd2f62eefee4f5b96d5758d7f09
fisher <tiebayu@126.com>
2022-07-24 23:42:00 +0800

    567file.com support

--- yunpan.meta.js
+++ yunpan.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         多网盘直接下载
 // @namespace    https://github.com/52fisher
-// @version      2.6
+// @version      2.7
 // @description  对一些网盘的功能进行重写，使得用户可以达到直接跳转下载页面、跳过验证码、免等待时间、兼容常见浏览器等功能.注意：无法实现会员下载，加速请支持正版
 // @author       fisher
 // @updateURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/yunpan.user.js
--- yunpan.user.js
+++ yunpan.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         多网盘直接下载
 // @namespace    https://github.com/52fisher
-// @version      2.6
+// @version      2.7
 // @description  对一些网盘的功能进行重写，使得用户可以达到直接跳转下载页面、跳过验证码、免等待时间、兼容常见浏览器等功能.注意：无法实现会员下载，加速请支持正版
 // @author       fisher
 // @updateURL    https://gitee.com/fisher52/Anti-CloudDisk/raw/master/yunpan.meta.js
 // @downloadURL  https://gitee.com/fisher52/Anti-CloudDisk/raw/master/yunpan.user.js
-// @match        https://www.567pan.com/*
-// @match        http://www.567pan.com/*
+// @match        https://www.567file.com/*
 // @match        http://www.xun-niu.com/*
 // @match        http://www.xiguapan.com/*
 // @match        http://www.520file.com/*
@@ -69,7 +68,7 @@
         'www.eos-53.com': function() { //520盘
             disk.zV2Public().hookClick();
         },
-        'www.567pan.com': function() { //567盘 自动跳转https
+        'www.567file.com': function() { //567盘 自动跳转https
             location.protocol == 'http:' ? location.href = 'https:' + location.href.substring(window.location.protocol.length) : null;
             disk.hookTimer();
             disk.pdzpublic();
